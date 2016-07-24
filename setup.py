42c1a27643f66c264291d0fdfea18b7be7fd2c0d
Koichi Nakamura <koichi@idein.jp>
2016-07-25 04:41:10 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'pretty',
+    version = '0.0.1',
+    author = 'Koichi Nakamura',
+    author_email = 'koichi@idein.jp',
+    description = "Philip Wadler's pretty printer for Python",
+    license = 'MIT',
+    url = 'https://github.com/nineties/pretty',
+    py_modules = ['pretty']
+    )
